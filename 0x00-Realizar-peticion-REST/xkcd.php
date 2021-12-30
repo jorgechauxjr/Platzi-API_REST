42ee0b1589c5c6aeceafcc64c15781d88b69c206
Jorge <chauxjorge@hotmail.com>
2021-12-30 12:53:03 -0500

    Get solo url de imagen

--- 0x00-Realizar-peticion-REST/xkcd.php
+++ 0x00-Realizar-peticion-REST/xkcd.php
@@ -1,3 +1,11 @@
 <?php
 
-echo file_get_contents('https://xkcd.com//info.0.json').PHP_EOL;
+// guardo string en variable json
+$json = file_get_contents('https://xkcd.com//info.0.json').PHP_EOL;
+
+// creo arreglo
+$data = json_decode( $json, true);
+
+// mostrar la propiedad img de los dato obtenidos
+// PHP_EOL es un enter
+echo $data['img'].PHP_EOL;
