5b111ff21f896803ca312386b0e28c5fad89d49e
Jorge <chauxjorge@hotmail.com>
2021-12-30 14:17:00 -0500

    Modify server.php modificar el get

--- 0x01-exponer-datos-by-HTTP-GET/server.php
+++ 0x01-exponer-datos-by-HTTP-GET/server.php
@@ -1,4 +1,21 @@
 <?php
+
+// Defino los recursos que serán posibles
+$allowedResourceTypes = [
+  'books',
+  'authors',
+  'genres'
+];
+
+// Validar si el tipo de recurso que me están solicitando está disponible o dentro del arreglo de recursos posibles
+
+$resourceType = $_GET['resource_type'];
+
+// si resourcetype no pertenece al arreglo allowed...
+if (!in_array( $resourceType, $allowedResourceTypes ) ) {
+  die;
+}
+
 switch ( strtoupper($_SERVER['REQUEST_METHOD']) ) {
   case 'GET':
     break;
